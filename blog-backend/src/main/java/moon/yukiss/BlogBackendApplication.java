4356bcbcda401d315b12f68e36ec1271bf328510
Marisa Kirisame <1130159276@qq.com>
2026-04-12 13:15:34 +0800

    feat: 初始化 Spring Boot 后端项目和 Hello 接口。

--- blog-backend/src/main/java/moon/yukiss/BlogBackendApplication.java
+++ blog-backend/src/main/java/moon/yukiss/BlogBackendApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogBackendApplication {
+
     public static void main(String[] args) {
-        // 这一行代码，唤醒整个 Spring Boot 生态
         SpringApplication.run(BlogBackendApplication.class, args);
-        System.out.println("====== 博客后端启动成功！ ======");
     }
+
 }
